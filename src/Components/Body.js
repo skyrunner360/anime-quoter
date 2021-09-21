4e56615118f135d405c4d0776ddc9bf1c3677245
skyrunner360 <mathurrishi31@gmail.com>
2021-09-21 11:06:41 +0530

    Basic UI Ready

---
+++ src/Components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Quotes from './Quotes'
+
+export default function Body() {
+    return (
+        <>
+            <Navbar/>
+            <Quotes/>
+        </>
+    )
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <header className="text-gray-400 bg-gray-900 body-font">
+  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+     
+    </nav>
+    <a href="/" className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-4 md:mb-0">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+      </svg>
+      <span className="ml-3 text-xl xl:block lg:hidden">Anime-Quoter</span>
+    </a>
+    <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+      <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Get a Random Quote
+      </button>
+    </div>
+  </div>
+</header>
+        </div>
+    )
+}
