fd5e7e3640c765ce180274f7afcf990518693de9
skyrunner360 <mathurrishi31@gmail.com>
2021-09-22 19:23:44 +0530

    fetching and setting data successful

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -4,19 +4,13 @@ import Quotes from './Quotes'
 
 export default function Body(props) {
     const [anime, setAnime] = useState([])
-    const [quote, setQuote] = useState([])
-    const [character, setCharacter] = useState([])
     // const [loading, setLoading] = useState(true)
 const fetchData= async()=>{
     const url=`https://animechan.vercel.app/api/quotes`
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    for(let i=0;i<10;i++){
-        setAnime(parsedData[i].anime)
-        setQuote(parsedData[i].quote)
-        setCharacter(parsedData[i].character)
-    }
+    setAnime(parsedData)
 }
 useEffect(() => {
     fetchData();
@@ -27,9 +21,13 @@ useEffect(() => {
             <div className="text-gray-400 bg-gray-900 body-font">
             <Navbar/>
             <h1 className="my-2 text-gray-100 bg-gray-900 body-font">Here are some Quotes from Animes for You</h1>
-                  <Quotes quotes={quote} character={character} animeName={anime} />
-
-          
+                 {anime.map(an=>{
+                     return (
+                            <div className="clo-md-4" key={an.quote}>
+                                <Quotes quotes={an.quote} character={an.character} animeName={an.anime} />
+                            </div>
+                     )
+                 })}
             </div>
         </>
     )
--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -4,8 +4,8 @@ export default function Quotes(props) {
   return (
     <>
       <section className="text-gray-400 bg-gray-900 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap -m-4">
+        <div className="px-5 py-24 mx-auto">
+          <div className="flex d-flex justify-content-center flex-wrap -m-4">
             <div className="p-4 lg:w-1/3">
               <div className="h-full bg-gray-800 bg-opacity-40 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
                 <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
