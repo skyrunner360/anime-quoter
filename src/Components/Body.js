456e6dfe0716a5ea4a23d5d4d4888719838500ab
skyrunner360 <mathurrishi31@gmail.com>
2021-09-24 08:38:52 +0530

    Loading Spinner Working

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,16 +1,20 @@
 import React,{useEffect,useState} from 'react'
 import Navbar from './Navbar'
 import Quotes from './Quotes'
+import Spinner from './Spinner'
 
 export default function Body(props) {
     const [anime, setAnime] = useState([])
+    const [loading, setLoading] = useState(true)
     // const [loading, setLoading] = useState(true)
 const fetchData= async()=>{
     const url=`https://animechan.vercel.app/api/quotes`
+    setLoading(true)
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
     setAnime(parsedData)
+    setLoading(false)
 }
 useEffect(() => {
     fetchData();
@@ -20,7 +24,8 @@ useEffect(() => {
         <>
             <div className="text-gray-400 bg-gray-900 body-font">
             <Navbar/>
-            <h1 className="my-2 text-gray-100 bg-gray-900 body-font">Here are some Quotes from Animes for You</h1>
+            <h1 className="my-2 text-center text-gray-100 bg-gray-900 body-font" style={{margin: '35px 0px', marginTop:'90px'}}>Here are some Quotes from Animes for You</h1>
+            {loading && <Spinner/>}
                  {anime.map(an=>{
                      return (
                             <div className="clo-md-4" key={an.quote}>
--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -3,7 +3,7 @@ import loading from './loading.gif'
 
 export default function Spinner() {
     return (
-        <div className="text-center">
+        <div className="text-center d-flex justify-content-center">
             <img className="my-3" src={loading} alt="Loading..." />
         </div>
     )
